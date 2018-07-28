00bab75e932a5a42e91a56bdbe1af3ff7298cff6
MYMQ <maike_bleach@hotmail.com>
2018-07-28 16:54:31 -0700

    small changes

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ import mongoose from "mongoose"
 import bodyParser from "body-parser"
 import exphbs from "express-handlebars"
 var app=express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', routes);
