e7c64a17f7489810001e85791180e92143ecd913
MYMQ <maike_bleach@hotmail.com>
2018-07-28 16:21:44 -0700

    small changes

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-// somethhing
-//else {
+import express from "express"
+import mongoose from "mongoose"
+import bodyParser from "body-parser"
+import exphbs from "express-handlebars"
+var app=express();
