147e015caea8ec6a7d7130d6263871feb861bb08
MYMQ <maike_bleach@hotmail.com>
2018-07-28 21:03:45 -0700

    orgnized

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var expressValidator = require('express-validator');
 var School = require('./models/school').School;
 var exphbs = require('express-handlebars');
 var mongoose = require('mongoose');
-var router = require('./routes/routes.js')
+var router = require('./routes/routes.js');
+var thais = require('./routes/thais.js');
 
 mongoose.connection.on('connected', function(){
   console.log('Connected to MongoDb');
@@ -17,15 +18,13 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 var app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, '/public')));
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-app.get('/', function(req, res){
-
-})
-
-app.use('/', router)
+app.use('/', router);
+app.use('/', thais);
 
 var port = process.env.PORT || 3000;
 
