2572b74067a4dbad57efb4768cce5029486153df
MYMQ <maike_bleach@hotmail.com>
2018-07-28 23:29:20 -0700

    top3

--- routes/mike.js
+++ routes/mike.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/list',function(req,res){
-  SCHEMA_NAME.find().populate(NAME_OF_OBJECTID).exec(function(error,data){
-    var newData=data.sort((a,b)=> (a.points+b.points));
-    newData.map((data)=>(data.NAME_OF_OBJECTID))
-    res.render('top3list', {"schools":newData});
+  InitialPoints.find().populate('school').exec(function(error,data){
+    var newData=data.sort((a,b)=> (a.totalPoints + b.totalPoints));
+    var objectOfData=newData.map((data)=>(data.NAME_OF_OBJECTID));
+    res.render('top3list', {"schools":ObjectOfData, 'commute':newdata.commute});
   })
 
 })
