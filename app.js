400675b40f82c3011ace502ad5e3d23eeec3f9e3
Diego Ibarra <dibarra@uchicago.edu>
2018-07-29 04:11:33 -0700

    Top three

--- app.js
+++ app.js
@@ -30,15 +30,6 @@ app.use('/', router);
 app.use('/', thais);
 app.use('/', kyra);
 
-translate('Ik spreek Engels', {to: 'en'}).then(res => {
-    console.log(res.text);
-    //=> I speak English
-    console.log(res.from.language.iso);
-    //=> nl
-}).catch(err => {
-    console.error(err);
-});
-
 var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var School = require('../models/school.js')
 
 
 router.get('/quiz', function(req, res){
@@ -10,8 +11,32 @@ router.post('/quiz', function(req, res){
   res.redirect('/map');
 })
 
-router.get('/map', function(req, res){
-  res.render('map');
+router.get('/map',function(req,res){
+ var school = new School({
+   "name": "St. Louise",
+   "level": "Elementary",
+   "langProg": ['Spanish', 'French'],
+   "start": "8 a.m.",
+   "end": "10 a.m.",
+   "afterSchoolProg": ['Basketball', 'Soccer'],
+   "demo": ['Black', '10%'],
+   "size": 400,
+   "test": [{Math:40, English:40}],
+   "address": "530 Shelby Lane",
+   "neighbor": "South Side",
+   "Link": ["https://www.stlouiseschool.org/"],
+   "imageUrl": "https://3.files.edl.io/2a02/18/07/18/133445-9b659130-6ea5-4e18-bb46-96dba953184d.jpg",
+ })
+ console.log(school.name)
+ console.log(school.level)
+ console.log(school.langProg[0])
+ console.log(school.start)
+ console.log(school.end)
+ console.log(school.afterSchoolProg)
+ var myScore = school.test[0].Math
+ console.log(school.test[0].Math)
+  res.render('top3list.hbs', {link: school.Link[0], score: myScore, schools: [school]
+  });
 })
 
 
